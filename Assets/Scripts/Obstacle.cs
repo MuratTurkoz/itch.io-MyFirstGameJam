5567c7c5012079a2591d477fb995f8d348823cf7
MuratTurkoz <44347576+MuratTurkoz@users.noreply.github.com>
2023-07-30 15:51:15 +0300

    Update Assets

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -6,7 +6,22 @@ public class Obstacle : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-            GameInstance.Instance.Lose();
+        if (collision.rigidbody.CompareTag("Player"))
+        {
+            Vector3 hitNormal = collision.GetContact(0).normal;
+            float hitDot = Vector3.Dot(hitNormal, Vector3.forward);
+            Debug.Log(hitNormal);
+
+            if (hitDot > 0.99f)
+            {
+                GameInstance.Instance.Lose();
+                //Destroy(collision.gameObject);
+            }
+            else
+            {
+                collision.rigidbody.AddForce(-hitNormal * 100*Time.deltaTime, ForceMode.Impulse);
+            }
+        }
+
     }
 }
